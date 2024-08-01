4465e46390edce8d305d85fbb10677da0bfc4fbd
unknown <balajidontulofficial@outlook.com>
2024-08-01 15:53:07 +0530

    cleaned

--- CloudGateway/src/main/java/com/personal/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/personal/CloudGatewayApplication.java
@@ -1,15 +1,7 @@
 package com.personal;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
-import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
@@ -22,15 +14,5 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
-	@Bean
-	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
-		return factory -> factory.configure(builder -> builder
-						.circuitBreakerConfig(CircuitBreakerConfig.custom()
-								.slidingWindowSize(10)
-								.failureRateThreshold(50)
-								.waitDurationInOpenState(Duration.ofSeconds(5))
-								.build())
-						.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()),
-				"orderCircuitBreaker");
-	}
+
 }
